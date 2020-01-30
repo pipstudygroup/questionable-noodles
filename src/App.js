376784d9cd90c226c20557bc72a9c8a2b7384598
jlsherrin <jay_sherrin@homedepot.com>
2020-01-30 16:36:16 -0600

    People....kinda....

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [currentLunchLocation, setCurrentLunchLocation] = useState('Click the Button');
+  const people = [
+    {
+      name: 'Jay',
+      hates: [
+        'meat',
+      ],
+      attending: false,
+    },
+    {
+      name: 'Raji',
+      hates: [
+        'Chipotle',
+      ],
+    },
+    {
+      name: 'K',
+      hates: [
+        'Bennihanas',
+      ],
+    },
+    {
+      name: 'Icon',
+      hates: [
+        'Demassi\'s',
+      ],
+    }
+  ];
   
+  const [currentLunchLocation, setCurrentLunchLocation] = useState('Click the Button');
+  const [attendees, setAttendees] = useState(people.map(person => person.name));
+  
+  const updateAttendees = (newPerson) => {
+    if(attendees.includes(newPerson)) {
+    
+    }
+    setAttendees();
+  }
+  
+  console.log('attendees', attendees);
   const restaurants = [
-    'Demassis',
-    'Benihanas',
-    'Perrys',
-    'Freebirds',
-    'Chipolte',
-    'La Burger'
-  ]
+    'Demassi\'s',
+    'Benihana',
+    'Perry\'s',
+    'Freebird\'s',
+    'Chipotle',
+    'LA Burger'
+  ];
+  
   const lunchLotto = () => {
     let randomIndex = Math.floor(Math.random() * restaurants.length);
     setCurrentLunchLocation(restaurants[randomIndex]);
-  }
+  };
 
   return (
     <div className="App">
@@ -23,6 +61,15 @@ const App = () => {
         {currentLunchLocation}
       </h2>
       <button onClick={()=>{ lunchLotto() }} style={{backgroundColor: 'purple', color: 'white'}}>Where to Lunch?</button>
+      <div>
+        {people.map( person => {
+          return (
+            <>
+              {person.name}<input type="checkbox" onChange={() => setAttendees()}/>
+            </>
+          )
+        })}
+      </div>
     </div>
   );
 }
