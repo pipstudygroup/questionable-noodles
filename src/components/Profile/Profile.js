4d53376ee94a890581662edc2b5eb92d0fc64b4b
kxk5533 <kalee_kuhn@homedepot.com>
2020-02-21 15:55:50 -0600

    whoops missed a few files

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const useSearch = () => {
+    return new URLSearchParams(useLocation().search);
+}
+
+export const Profile = (props) => {
+    const search = useSearch();
+    return (
+        <>
+            <div>I am a Profile Page! HI</div>
+            <div>{search.get('name')}</div>
+        </>
+    )
+}
