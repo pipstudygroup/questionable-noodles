6394e31a97ab992d634df475f78f6c31f1fb8a44
kxk5533 <kalee_kuhn@homedepot.com>
2020-02-21 15:55:02 -0600

    Router scaffolding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Fragment, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import LunchLotto from './pages/LunchLotto';
+import { Profile } from './components/Profile/Profile';
 
-const App = () => {
-
+const App = (props) => {
+  console.log(props);
   return (
     <div className="App">
-      < LunchLotto />
+      <Router>
+        <Switch>
+          <Route path='/' exact component={LunchLotto} />
+          <Route path='/profile/:name' component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
